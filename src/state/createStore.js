fd59f67c9778cc92547ac98a0b031681632a39ab
mark <markcorupe@gmail.com>
2021-05-06 15:56:12 -0700

    some smart ass

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -1,7 +1,13 @@
+//idk this was used in an example, they did a few things like all at the same time and it confused me. this is like a reducer
+//and action creator all in one.
+//Idk i think this is like you know how we did the Fizzbuzz thing in the bootcamp... and then some smart ass out there can do it in one line
+//this is like that, some smart ass did a redux thing in one line and idk its magic or something.
+
+
 import { createStore as reduxCreateStore } from " redux"
 
 
-const reducer = (state, actoin) => {
+const reducer = (state, action) => {
     if (actoin.type === `INCREMENT`) {
         return Object.assign({}, state, {
             count: state.count +1,
